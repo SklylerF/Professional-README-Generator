05cbb5f66fa9cb44d210f1b48832a672d303fe7f
Skyler Ferreira <sky_3489@icloud.com>
2022-07-20 15:27:36 -0700

    working readme generator with dynamically generated read me

--- Develop/index.js
+++ Develop/index.js
@@ -1,13 +1,94 @@
 // TODO: Include packages needed for this application
-
+const inquirer = require('inquirer');
+const fs = require('fs')
+const generateMarkdown = require("./utils/generateMarkdown");
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = () => {
+    const userQuestions = inquirer.prompt([
+      {
+        type: 'input',
+        message: "what's the title for your project?",
+        name: "title",
+        //Ensures the user has included a title for their README
+        validate: userTitle => {
+          if (userTitle) {
+            return true;
+          } else {
+            console.log("Project title is required!")
+            return false;
+          }
+        }
+      },
+      {
+        type: 'input',
+        message: "Give your project a description:",
+        name: 'description'
+      },
+      {
+        type: 'input',
+        message: "How to install your project?",
+        name: 'installation'
+      },
+      {
+        type: 'input',
+        message: "What is the usage for this project?",
+        name: 'usage'
+      },
+      {
+        type: 'input',
+        message: 'what is your aplication contributing to?',
+        name: 'contribution'
+      },
+      {
+        type: "checkbox",
+        message: "choose your licenses if any.",
+        name: 'licenses',
+        choices: ["Apache", "GNU", "ISC", "MIT", "OBSD", "Public Domain (No license)"]
+      },
+      {
+        type: 'input',
+        message: 'what are the test instructions?',
+        name: 'tests',
+      },
+      {
+        type: "input",
+        message: "What is your Github username?",
+        name: 'github',
+      },
+      {
+        type: "input",
+        message: "What is your email?",
+        name: 'email',
+      },
+    ])
+    return userQuestions
+  }
+//   writes in read me 
+function writeToFile(readMe) {
+    return new Promise((resolve, reject) => {
+      //Generates README into the 'generated' folder
+      fs.writeFile("./generated/README.md", readMe, err => {
+        if (err) {
+          console.log("Error writing to file");
+          reject(err);
+          return;
+        }
+        resolve()
+      })
+    })
+  };
 
-// TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
-
-// TODO: Create a function to initialize app
-function init() {}
-
-// Function call to initialize app
-init();
+  questions()
+//   generates the mark down 
+.then(data => {
+    return generateMarkdown(data)
+  })
+//   creates README
+.then(readMe => {
+    writeToFile(readMe);
+    console.log("README successfully generated!");
+  })
+//   errors 
+.catch(err => {
+    console.log(err)
+  })
--- Develop/utils/generateMarkdown.js
+++ Develop/utils/generateMarkdown.js
@@ -1,20 +1,65 @@
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
-function renderLicenseBadge(license) {}
+function renderLicenseBadge(license) {
+  license = license.map(badge => {
+    if (badge !== "Public Domain (No license)") {
+      return `![License](https://img.shields.io/static/v1?label=License&message="${badge}&color=BLUE)`
+    }
+  })
+  return license.join(" ")
+}
 
-// TODO: Create a function that returns the license link
-// If there is no license, return an empty string
-function renderLicenseLink(license) {}
+function renderListObject(licenseObject) {
+  licenseObject = licenseObject.map(item => "* " + item)
+  return licenseObject.join(" \n")
+}
 
-// TODO: Create a function that returns the license section of README
-// If there is no license, return an empty string
-function renderLicenseSection(license) {}
-
-// TODO: Create a function to generate markdown for README
+//Generates a README using info from index.js
 function generateMarkdown(data) {
-  return `# ${data.title}
+  //changes values 
+  const {title, description, installation, usage, contribution, licenses, tests, github, email} = data;
 
+  //Returns the completed README
+  return `
+  
+# ${title}
+
+${renderLicenseBadge(licenses)}
+
+## Description
+${description}
+
+## Table of Contents
+* [Installation](#installation)
+* [Usage](#usage)
+* [Licenses](#licenses)
+* [Contribution](#contribution)
+* [Tests](#tests)
+* [Questions](#questions)
+
+## Installation
+${installation}
+
+## Usage
+${usage}
+
+## Licenses
+This project falls under the following license(s): \n
+${renderListObject(licenses)}
+
+## Contribution
+${contribution}
+
+## Tests
+${tests}
+
+## Questions
+If you have any questions, please contact me below: \n
+
+Github: [${github}](https://github.com/${github}) \n
+Email: ${email}
 `;
 }
+
 
 module.exports = generateMarkdown;
